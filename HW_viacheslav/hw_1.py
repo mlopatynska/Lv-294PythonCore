47bc476225e40493827c93749fd03a4a6687e2df
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-01-28 18:59:35 +0200

    HW1

---
+++ HW_viacheslav/hw_1.py
@@ -0,0 +1,19 @@
+name = raw_input(
+				"What is your name?\n"
+								)
+	
+age = raw_input(
+				"Haw old are you?\n"
+						 	)
+	
+live_place = raw_input(
+				"Where are you live?\n"
+									    )
+	
+print ('Hello,' + name \
+		  + '!\nYour age is ' \
+          + age + '.\nYou live in ' \
+          + live_place + '.'
+          ) 
+
+
