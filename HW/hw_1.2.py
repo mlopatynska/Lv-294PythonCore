bffbb0e49d02ef90c0aa8f2b0e933275f80ab5bd
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-01-29 12:38:45 +0200

    My hw 1.2

---
+++ HW/hw_1.2.py
@@ -0,0 +1,21 @@
+from math  import  pow
+
+a = input("Input first number:\n")
+
+b = input("Input second number:\n")
+
+print ('a - b : ' + str(a - b) \
+	   + '\na + b : ' + str(a + b) \
+	   + '\na * b : ' + str(a * b))
+
+try:
+	print('a / b : ' + str(float(a) / b))
+
+except ZeroDivisionError:
+	print('You can not divide by zero!!!')
+
+else:
+	print('a // b : ' + str(a // b) \
+		  + '\na % b : ' + str(a % b))
+
+print('a ^ b : ' + str(pow(a,b)))
