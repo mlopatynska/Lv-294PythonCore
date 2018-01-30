a504a8ecbb33cd77bbbd6d56d7c08800eee9cd81
Viacheslav Prykhod'ko <vpryktc@training.local>
2018-01-30 20:33:09 +0200

    HW 2.3

---
+++ HW/hw_2.3.py
@@ -0,0 +1,9 @@
+a = input("Input a = ")
+
+b = input('Input b = ')
+
+print("Not real swap(a and b don't change:\na = {}\nb = {}".format(b,a))
+
+a ,b = b , a
+
+print("Swap a and b:\na = {}\nb = {}".format(a,b))
