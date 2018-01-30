d8da32a1d66f89a3c9e86e9a29bd1444d12c618f
Viacheslav Prykhod'ko <vpryktc@training.local>
2018-01-30 19:46:15 +0200

    Chenge 1.1

--- HW/hw_1.1.py
+++ HW/hw_1.1.py
@@ -3,8 +3,10 @@ name = raw_input("What is your name?\n")
 age = raw_input("Haw old are you?\n")
 	
 live_place = raw_input("Where are you live?\n")
-	
+
+salary = input("What is your salary?\n")
+
 print ('Hello, {}!\nYour age is {}\
-       .\nYou live in {}.'.format(name,age,live_place))
+       .\nYou live in {}.\nYour salary {:.2f}'.format(name,age,live_place,salary))
 
 
