7aed12debfe6013d9843bf69ae6e50fcb45da1e7
Viacheslav Prykhod'ko <vpryktc@training.local>
2018-01-30 19:30:46 +0200

    Change

--- HW/hw_1.1.py
+++ HW/hw_1.1.py
@@ -4,9 +4,7 @@ age = raw_input("Haw old are you?\n")
 	
 live_place = raw_input("Where are you live?\n")
 	
-print ('Hello,' + name \
-	   + '!\nYour age is ' \
-       + age + '.\nYou live in ' \
-       + live_place + '.')
+print ('Hello, {}!\nYour age is {}\
+       .\nYou live in {}.'.format(name,age,live_place))
 
 
