ab69a1f73c2118718498e26335d7c280c86a14ef
Andrii Yuschyk <ayusctc@training.local>
2018-02-01 19:58:22 +0200

    Task where we heeded to swop veriables

--- 4.py
+++ 4.py
@@ -3,4 +3,3 @@ b=2
 a,b=b,a
 print a
 print b
-
---
+++ something.py
@@ -0,0 +1,6 @@
+a = raw_input("Please, enter variable 1 \n")
+b = raw_input("Please, enter variable  2 \n")
+a,b=b,a
+print "Now we are going to change your variables :)"
+print "veriable  1 = ",a
+print "veriable  2 = ",b
