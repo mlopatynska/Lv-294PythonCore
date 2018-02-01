0b8a18b688de47e499df5c9c80bc3be70922d077
Andrii Yuschyk <ayusctc@training.local>
2018-02-01 20:16:26 +0200

    upgrate of hometask

--- task2.py
+++ task2.py
@@ -1,5 +1,6 @@
 a = int(raw_input("Enter some number from 1000 to 9999:  \n"))
 b=list(str(a))
+b.sort()
 
 p1=a%10
 a=a//10
@@ -12,4 +13,4 @@ p4=a%10
 
 print "The sum of number is", p1+p2+p3+p4
 print "Reverse is", p1,p2,p3,p4
-print b
+print "Values from the smalest to the largest "," ".join(b)
