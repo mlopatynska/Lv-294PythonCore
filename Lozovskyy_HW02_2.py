e0186cf86fcef17991464be1dca45878e1523b45
Taras Lozovs'kyi <tlozotc@training.local>
2018-02-06 18:25:04 +0200

    HomeWork 02_02 updated. Multiplication of digit done

--- Lozovskyy_HW02_2.py
+++ Lozovskyy_HW02_2.py
@@ -1,6 +1,13 @@
-x = 5678
+x = input ("Введіть 4-значне число, будь ласка: ")
+// приводимо число в текст
 y = str(x)
+// Пошук суми цифр числа
 sum = int (y[0]) + int (y[1]) + int (y[2]) + int (y[3])
+print ("Сума цифр цього числа складе: ")
 print sum
-sum = int ((str(x))[0]) + int ((str(x))[1]) + int ((str(x))[2]) + int ((str(x))[3])
-print sum
+// Пошук добутку цифр
+mult = int (y[0]) * int (y[1]) * int (y[2]) * int (y[3])
+print ("Result of multiplication: " + mult )
+// Відображення в реверсному подярку
+print y[::-1]
+
