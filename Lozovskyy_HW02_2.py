3a7224e539d04b181fbd1fa07e4dcf6ceae46040
Taras Lozovskyy <lozovskyy.taras@gmail.com>
2018-02-06 15:40:11 +0200

    second homework added

---
+++ Lozovskyy_HW02_2.py
@@ -0,0 +1,6 @@
+x = 5678
+y = str(x)
+sum = int (y[0]) + int (y[1]) + int (y[2]) + int (y[3])
+print sum
+sum = int ((str(x))[0]) + int ((str(x))[1]) + int ((str(x))[2]) + int ((str(x))[3])
+print sum
---
+++ Lozovskyy_HW02_3.py
@@ -0,0 +1,8 @@
+x = 1
+y = 2
+print x
+print y
+x,y = y,x
+print x
+print y
+
