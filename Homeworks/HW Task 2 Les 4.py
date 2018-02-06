d8c4a675d2ec0fab7b8b1b01e896ea2e20ff4ed8
Ostap Levyts'kyi <olevyttc@training.local>
2018-02-06 21:09:24 +0200

    Task 1,2 Les 4

---
+++ Homeworks/HW Task 2 Les 4.py
@@ -0,0 +1,17 @@
+day = input ("Enter day \n")
+mouth = input ("Enter mouth \n")
+year = input ("Enter year \n")
+if day > 0 and day < 32:
+    print ("Day entered correct")
+else:
+    print ("Day entered incorrect")
+
+if mouth > 0 and mouth < 13:
+    print ("Mouth entered correct")
+else:
+    print ("Mouth entered incorrect")
+
+if year > 0 and year < 2019:
+    print ("Year entered correct")
+else:
+    print ("Year entered incorrect")
