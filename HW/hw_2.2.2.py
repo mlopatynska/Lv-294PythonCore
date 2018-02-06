71f738e0a9e6f5e1295540b9f44d4371ee7a1975
Viacheslav Prykhod'ko <vpryktc@training.local>
2018-02-06 18:20:35 +0200

    pro!!!+

--- HW/hw_2.2.2.py
+++ HW/hw_2.2.2.py
@@ -4,39 +4,39 @@ number = input("Input four-digit number:\n")
 number = str(number)
 
 #The firs way, work for all integer number above zero.
-sum_of_number = 0
+sum_of_number = 1
 
-sum_of_number += number.count("1") * 1
-sum_of_number += number.count("2") * 2
-sum_of_number += number.count("3") * 3
-sum_of_number += number.count("4") * 4
-sum_of_number += number.count("5") * 5
-sum_of_number += number.count("6") * 6
-sum_of_number += number.count("7") * 7
-sum_of_number += number.count("8") * 8
-sum_of_number += number.count("9") * 9
+sum_of_number *= number.count("1") * 1
+sum_of_number *= number.count("2") * 2
+sum_of_number *= number.count("3") * 3
+sum_of_number *= number.count("4") * 4
+sum_of_number *= number.count("5") * 5
+sum_of_number *= number.count("6") * 6
+sum_of_number *= number.count("7") * 7
+sum_of_number *= number.count("8") * 8
+sum_of_number *= number.count("9") * 9
 
 print("Sum of number.\nThe first way: {}".format(sum_of_number))
 
 #Second way, only in four-digit integer number.
 number = int(number)
-sum_of_number = 0
+sum_of_number = 1
 
-sum_of_number += number % 10
-sum_of_number += number // 10 % 10
-sum_of_number += number // 100 % 10
-sum_of_number += number // 1000 % 10
+sum_of_number *= number % 10
+sum_of_number *= number // 10 % 10
+sum_of_number *= number // 100 % 10
+sum_of_number *= number // 1000 % 10
 
 print("The second way: {}\n".format(sum_of_number))
 
 #Third way.
 number = str(number)
-sum_of_number = 0
+sum_of_number = 1
 
-sum_of_number += int(number[0])
-sum_of_number += int(number[1])
-sum_of_number += int(number[2])
-sum_of_number += int(number[3])
+sum_of_number *= int(number[0])
+sum_of_number *= int(number[1])
+sum_of_number *= int(number[2])
+sum_of_number *= int(number[3])
 
 print("The third way: {}\n".format(sum_of_number))
 
