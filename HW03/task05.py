79f38617f01bb0210e6a06847f83af7514b806f3
enjeir <35846698+enjeir@users.noreply.github.com>
2018-02-09 09:49:36 +0200

    Finished 5th task

--- HW03/task05.py
+++ HW03/task05.py
@@ -1,2 +1,7 @@
-daynum = input('Enter the number between 1 and 365:\n')
-countday = daynum % 7
+daynum = int(input('Enter the number between 1 and 365:\n'))
+if 1 <= daynum <= 365:
+  countday = (daynum % 7) - 1
+  dayslist = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  print('The corresponding day for entered number will be {}'.format(dayslist[countday]))
+else:
+  print('You\'ve entered wrong number!')
