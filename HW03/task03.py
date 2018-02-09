da4e716bfa190c55472052ee9e75ec2b4a3255cf
enjeir <35846698+enjeir@users.noreply.github.com>
2018-02-09 09:21:26 +0200

    Made some cosmetic repairs

--- HW03/task03.py
+++ HW03/task03.py
@@ -5,12 +5,11 @@ c = float(input("c = "))
 
 discr = b ** 2 - 4 * a * c;
 if discr > 0:
-    import math
-    x1 = (-b + math.sqrt(discr)) / (2 * a)
-    x2 = (-b - math.sqrt(discr)) / (2 * a)
-    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+    x1 = (-b + discr ** 0.5) / (2 * a)
+    x2 = (-b - discr ** 0.5) / (2 * a)
+    print("x1 = {:.2f} \nx2 = {:.2f}".format(x1, x2))
 elif discr == 0:
     x = -b / (2 * a)
-    print("x = %.2f" % x)
+    print("x = {:.2f}".format(x))
 else:
     print("There are no solutions")
