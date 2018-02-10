0c371121e9401787ec81774f0a18f3144176574f
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-10 09:51:53 +0200

    rl HW 4.2

--- HW/hw_4.2.py
+++ HW/hw_4.2.py
@@ -1,8 +1,21 @@
                      #Task #2
+from calendar import monthrange
+from time import localtime
+
 year = input("Input year:\n")
 month = input("Input month:\n")
 day = input("Input day:\n")
 
-print "Year entered correctly!" if year > 0 else "Year entered not  correctly!"
-print "Month entered correctly!" if  1 <= month <= 12 else "Month  entered not correctly!"
-print "Day entered correctly!" if 1 <= day <=31 else "Day  entered not correctly!"
+t = (year, month, day)
+t_loc = localtime()
+t_loc = (t_loc[0], t_loc[1], t_loc[2])
+
+if (year > 0 and 1 <= month <= 12
+    and (1 <= day <= monthrange(year,month)[1])):
+    print("Date entered correctly!\n")
+    if t == t_loc:
+        print("The specified date is the same as the date on the PC")
+else:
+    print("Date entered incorrectly!")
+
+
