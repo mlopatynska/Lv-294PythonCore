6b4536b419a1610451a409d0fc502bf89ff23c7e
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-10 08:13:52 +0200

    rl HW 4.3

--- HW/hw_4.3.py
+++ HW/hw_4.3.py
@@ -1,12 +1,18 @@
                      #Task #3
+from math import sqrt
+
 a = input("Input a = ")
 b = input("Input b = ")
 c = input("Input c = ")
 
-D =  pow(b,2) - 4*a*c #Discriminator
+D = pow(b,2) - 4*a*c #Discriminator
 
 if not D:
-       x = (-b) / 2*a
-       print("X = {}".format(x))
+    x = (-b) / 2*a
+    print("X = {}".format(x))
+elif D > 0:
+    x1 = (-b + sqrt(D)) / 2 * a
+    x2 = (-b - sqrt(D)) / 2 * a
+    print("X1 = {}\nX2 = {}".format(x1,x2))
 else:
-       print("D not 0!")
+    print("There are no real roots.")
