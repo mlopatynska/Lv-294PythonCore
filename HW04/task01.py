00a3f48ace57865f0a340f5bc414c223af72f0ed
enjeir <35846698+enjeir@users.noreply.github.com>
2018-02-13 11:16:21 +0200

    Updated 4-digids task

--- HW04/task01.py
+++ HW04/task01.py
@@ -1,10 +1,11 @@
 checknum = int(input('Enter your number so I could find something for you:\n'))
-number = 999
-while number < 9999:
-    number += 1
-    num = str(number)
-    sumnumber = (int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]))
-    if sumnumber == checknum:
-        print number
+if 0 < checknum < 37:
+  number = 999
+  while number < 9999:
+      number += 1
+      num = str(number)
+      sumnumber = (int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]))
+      if sumnumber == checknum:
+          print number
 else:
-    print 'That\'s are all 4 digit numbers which sums are eqal to the number'
+    print 'For this value there are no 4-digit numbers which sum of digits would be equal to enterd value'
