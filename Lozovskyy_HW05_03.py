84c8d2956dfb092424ca24d72869d24e24df0cb0
Taras Lozovskyy <lozovskyy.taras@gmail.com>
2018-02-13 15:54:53 +0200

    HW05_03 adn 05_02 sorted

--- Lozovskyy_HW05_03.py
+++ Lozovskyy_HW05_03.py
@@ -1,14 +1,8 @@
-num = 0
-# Enter number and check is that number more than 0
-while num < 1:
-    num = int(input ("Enter number more than Zero "))
+a = 0
+b = 1
+num = int(input("Enter number: "))
+while a in range (0,num+1):
+    print "{}".format(a)
+    a, b = b, a + b
 else:
-    print "Programm we'll check is the {} simple number".format(num)
-# Check is an entered number Prime.
-for i in range(2,num):     #to iterate on the factors of the number
-    if num%i == 0:            #to determine the first factor
-         print "Entered number {} is NOT prime".format(num)
-         break
-    else:                                # else part of the loop
-        print "{} is a prime number!".format(num)
-        break
+    print "that's all digints in range"
