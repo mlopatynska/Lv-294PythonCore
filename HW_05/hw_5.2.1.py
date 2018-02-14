505e5943fb39f1f0e4da8456ccdee61cb2338e01
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-14 19:27:35 +0200

    midified HW 5.1 min elem

--- HW_05/hw_5.2.1.py
+++ HW_05/hw_5.2.1.py
@@ -1,35 +1,38 @@
 from random import randint
 
-num = []
-for i in range(8):
-    num.append(randint(0,100))
-
+num = [randint(0,100) for i in xrange(10)]
 print(num)
 
-#for - elemm
-min = num[0]
-for i in num:
-    if min > i:
-        min = i
 
-print("Min element (for - element) = {}\n".format(min))
+#for - elemm
+def for_elem(num):
+    min = num[0]
+    for i in num:
+        if min > i:
+            min = i
+    print("Min element (for - element) = {}\n".format(min))
 
 #for - index
-min = num[0]
-for i in range(1,len(num)):
-    if min > num[i]:
-        min = num[i]
-
-print("Min element (for - index) = {}\n".format(min))
+def for_index(num):
+    min = num[0]
+    for i in range(1,len(num)):
+        if min > num[i]:
+            min = num[i]
+    print("Min element (for - index) = {}\n".format(min))
 
 #while
-min = num[0]
-i = 1
-while i < len(num):
-    if min > num[i]:
-        min = num[i]
-    i += 1
-
-print("Min element (while) = {}\n".format(min))
 
 
+def while_index(num):
+    min = num[0]
+    i = 1
+    while i < len(num):
+        if min > num[i]:
+            min = num[i]
+        i += 1
+    print("Min element (while) = {}\n".format(min))
+
+
+for_elem(num)
+for_index(num)
+while_index(num)
