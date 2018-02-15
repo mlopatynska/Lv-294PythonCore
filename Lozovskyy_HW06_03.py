bfc26ee6fa5a2b3718bf884f848d58fad083ee6e
Lozovskyy <lozovskyy.taras@gmail.com>
2018-02-16 01:12:52 +0200

    HomeWork 06_03 Made with loops. Don't know how to do with comprehensions in hash.

--- Lozovskyy_HW06_03.py
+++ Lozovskyy_HW06_03.py
@@ -1,3 +1,27 @@
 data = range(100,10000,7)
 hash = {"min":10000, "max":100, "avg":0}
-hash = {"min": for i in data: if hash["min"]<=i }
+# hash = {"min": for i in data: if hash["min"]<=i }
+
+
+
+min = data[-1]
+max = data[0]
+count = 0
+sum = 0
+# Method 1 "for if"
+for i in data:
+    count += 1
+    sum += int(i)
+    if min >= int(i):
+        min = int(i)
+    elif max <= int(i):
+        max = int(i)
+avg = sum/count
+
+hash = {"min":min, "max":max, "avg": avg}
+
+print "Sum of digits: {}".format(sum)
+print "Minimum valuet: {}".format(min)
+print "Maximum value: {}".format (max)
+print "Average of input: {}".format(avg)
+print hash
