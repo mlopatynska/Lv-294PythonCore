3dc46c98b563c55cf3c811ce53af67c8caa0e27f
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-15 08:17:31 +0200

    modified HW 5.2 create dict

--- HW_05/hw_5.2.2.py
+++ HW_05/hw_5.2.2.py
@@ -1,11 +1,18 @@
+import collections
 word = raw_input("Write you love to fruits:\n")
-count_dict = {}
 
-for i in word:
-    if i not in count_dict:
-        count_dict[i] = 0
-    count_dict[i] +=1
+def count_d(word):
+    count_dict = {}
+    for i in word:
+        if i not in count_dict:
+            count_dict[i] = 0
+        count_dict[i] += 1
+    print(count_dict)
 
+
+count_d(word)
+
+print(collections.Counter(word))
+
+count_dict = {i:word.count(i) for i in word}
 print(count_dict)
-
-
