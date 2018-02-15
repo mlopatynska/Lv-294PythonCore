0b957ea2a6149cd45fe70012112f2641de55b6ae
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-15 17:22:54 +0200

    modified HW 5 Amstrong number

--- HW_05/hw_5.py
+++ HW_05/hw_5.py
@@ -23,3 +23,28 @@ if length_edge % 2:
     isosceles(length_edge)
 else:
     rectangular(length_edge)
+
+def all_amstrong_number():
+    n = input("Enter the search boundary:")
+    for i in xrange(0, n):
+        i = str(i)
+        x = len(i)
+        z = 0
+        for j in i:
+            z += pow(int(j),x)
+        if int(i) == z:
+            print(z)
+
+def amstrong_number():
+    n = input("Enter the number for check:")
+    x = str(n)
+    summ_n = 0
+    for i in x:
+        summ_n += pow(int(i),len(x))
+    if n == summ_n:
+        print("It's Amstrong number!")
+    else:
+        print("Not Amstrong number!")
+
+#all_amstrong_number()
+#amstrong_number()
