a3227c06a47dbfc493f7eea643e573523749688e
enjeir <35846698+enjeir@users.noreply.github.com>
2018-02-16 17:02:50 +0200

    Unfinished..

--- HW05/task01.py
+++ HW05/task01.py
@@ -1,1 +1,10 @@
-
+list = [22, 11, 6, 87, 34, 55, 74, 2, 109]
+min, max = list[0], list[0]
+for i in list:
+  if i < min:
+    min = i
+for i in list:
+  if i > max:
+    max = i
+dict = {'min': min, 'max': max}
+print dict
