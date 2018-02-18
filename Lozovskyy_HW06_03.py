fa555b311be759428ed96f4bb2a17f92d7f2a6d3
Lozovskyy <lozovskyy.taras@gmail.com>
2018-02-18 23:35:55 +0200

    Can't do with comprehentions in hash... made shortest version with for cycles.

--- Lozovskyy_HW06_03.py
+++ Lozovskyy_HW06_03.py
@@ -1,27 +1,36 @@
 data = range(100,10000,7)
-hash = {"min":10000, "max":100, "avg":0}
-# hash = {"min": for i in data: if hash["min"]<=i }
+hash = {"min":data[-1], "max":data[0], "avg":(sum(data)/len(data))}
+x = 100000
+# hash ["min"] = (i for i in data if hash["min"]>i)
 
-
-
-min = data[-1]
-max = data[0]
-count = 0
-sum = 0
-# Method 1 "for if"
+#Version 2
 for i in data:
-    count += 1
-    sum += int(i)
-    if min >= int(i):
-        min = int(i)
-    elif max <= int(i):
-        max = int(i)
-avg = sum/count
+    if hash["min"]>i:
+        hash["min"]=i
+    elif hash["max"]<i:
+        hash["max"]=i
 
-hash = {"min":min, "max":max, "avg": avg}
+#Version 1
+# min = data[-1]
+# max = data[0]
+# count = 0
+# sum = 0
+# # Method 1 "for if"
+# for i in data:
+#     count += 1
+#     sum += int(i)
+#     if min >= int(i):
+#         min = int(i)
+#     elif max <= int(i):
+#         max = int(i)
+# avg = sum/count
 
-print "Sum of digits: {}".format(sum)
-print "Minimum valuet: {}".format(min)
-print "Maximum value: {}".format (max)
-print "Average of input: {}".format(avg)
-print hash
+# hash = {"min":min, "max":max, "avg": avg}
+
+# print "Sum of digits: {}".format(sum)
+# print "Minimum valuet: {}".format(min)
+# print "Maximum value: {}".format (max)
+# print "Average of input: {}".format(avg)
+print "Hash info: {}".format(hash)
+print "Minimum value is: {}\nMaximum value is: {}\nAverage value is: {}"\
+    .format(hash["min"],hash["max"],hash["avg"])
