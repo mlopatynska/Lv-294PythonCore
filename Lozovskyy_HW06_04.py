c6da830bf0e065916783877753aa6665d1225a1e
Lozovskyy <lozovskyy.taras@gmail.com>
2018-02-18 23:03:42 +0200

    I understood the program well. Thank you!

--- Lozovskyy_HW06_04.py
+++ Lozovskyy_HW06_04.py
@@ -1,5 +1,5 @@
-# Don.t understud how it work's well
-n = 7
-for i in range(1,n + 1,2):
+# Painting triangle
+n = 10
+for i in range(1,(n + 1),2):
    x = (n-i)/2
    print('{0}{1}{0}'.format(' '*x,'*'*i))
