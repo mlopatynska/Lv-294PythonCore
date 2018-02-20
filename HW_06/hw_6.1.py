244e483c0fee40a886630e8ba66b1c450668a32d
Вячеслав Приходько <prihodko.v.r@gmail.com>
2018-02-20 13:34:18 +0200

    modifiel HW 6.1

--- HW_06/hw_6.1.py
+++ HW_06/hw_6.1.py
@@ -1,1 +1,23 @@
+from random import randint
 
+
+def my_multiplication(mas,var):
+    summ = 0
+    length = len(mas)
+    for i in range(length):
+        summ = mas[i] * pow(var,length - i)
+    return summ
+
+
+a = [i for i in xrange(1,62,2)]
+b = [i for i in xrange(2,63,2)]
+c = [i for i in xrange(31)]
+x = randint(1,100)
+y = randint(1,100)
+z = randint(1,100)
+
+ans = pow(my_multiplication(a,x),2) - (my_multiplication(b,y)) / \
+      my_multiplication(c,(x+z))
+
+print("a:\n{}\nb:\n{}\nc:\n{}\nx = {}\ny = {}\nz = {}".format(a, b, c, x, y, z))
+print("Ansver = {}".format(ans))
