b4c4c9a2840cbb897de46519632f5edda7cd3f67
enjeir <35846698+enjeir@users.noreply.github.com>
2018-02-27 18:02:23 +0200

    Finished factorial

--- HW06/task02.py
+++ HW06/task02.py
@@ -1,11 +1,11 @@
 n = input('Enter n value:\n')
 m = input('Enter m value:\n')
  
-def fucktorial(x):
+def factorial(x):
   prod = 1
   for i in range (1,x+1):
     prod *= i
   return prod
  
-result = fucktorial(n) * fucktorial(m) / fucktorial(n+m)
+result = factorial(n) * factorial(m) / float(factorial(n+m))
 print('F(m,n) = {:.10f}'.format(result))
