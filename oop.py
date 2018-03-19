895c534e134a49ebf1774ddfce502afdcba9acaf
Lozovskyy <33579674+Lozovskyy@users.noreply.github.com>
2018-03-19 23:06:05 +0200

    oop bubbles homework

---
+++ oop.py
@@ -0,0 +1,57 @@
+import pygame
+from random import randint
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Lozovskyy's_OOP")
+clock = pygame.time.Clock()
+
+done = False
+
+class Rectangle():
+    def __init__(self):
+        self.x = randint(0,780)
+        self.y = randint(0,530)
+        self.wight = randint(20,70)
+        self.length = randint(20,70)
+        self.color = [randint(0,255) for i in xrange(3)]
+
+
+    def draw(self):
+        pygame.draw.rect(screen,self.color,
+                         [self.x,self.y,self.wight,self.length])
+
+
+class Elipse(Rectangle):
+
+    def draw(self):
+        pygame.draw.ellipse(screen,self.color,
+                            [self.x, self.y, self.wight, self.length])
+
+
+
+
+my_list = []
+for i in xrange(500):
+    my_object = Rectangle()
+    my_list.append(my_object)
+for j in xrange(500):
+    my_object = Elipse()
+    my_list.append(my_object)
+
+
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    for i in my_list:
+        i.draw()
+    pygame.display.flip()
+    clock.tick(10)
+
+
+
+pygame.quit()
+quit()
